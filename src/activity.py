0f48c46eea81fc64d6a9fb418eacaf36a3d2691b
tompass8 <tom.passerman@ynov.com>
2025-10-30 11:23:35 +0100

    feat: ajout des fonctions hunt et fish avec vérification des zones

--- src/activity.py
+++ src/activity.py
@@ -1,25 +1,28 @@
 import random
 from src.item import Item
 
-def hunt(player):
-    print("🏹 Vous partez chasser...")
-    success = random.random() < 0.6
-    if success:
+def hunt(player, current_room):
+    if not current_room.can_hunt:
+        print(" Vous ne pouvez pas chasser ici.")
+        return
+    print(" Vous partez chasser...")
+    if random.random() < 0.6:
         meat = Item("Viande", "food", heal=20)
         player.add_item(meat)
-        print("🦌 Vous avez chassé avec succès et obtenu de la viande !")
+        print(" Vous avez chassé avec succès et obtenu de la viande !")
     else:
-        print("🐾 Vous n'avez rien trouvé cette fois...")
+        print(" Vous etes trop nul. hahaha...")
     player.decrease_stats()
 
-
-def fish(player):
-    print("🎣 Vous tentez de pêcher...")
-    success = random.random() < 0.5
-    if success:
-        fish = Item("Poisson", "food", heal=15)
-        player.add_item(fish)
-        print("🐟 Vous avez attrapé un poisson !")
+def fish(player, current_room):
+    if not current_room.can_fish:
+        print(" Vous ne pouvez pas pêcher ici.")
+        return
+    print(" Vous tentez de pêcher...")
+    if random.random() < 0.5:
+        fish_item = Item("Poisson", "food", heal=15)
+        player.add_item(fish_item)
+        print(" Vous avez attrapé un poisson !")
     else:
-        print("🌊 Rien n’a mordu à l’hameçon...")
+        print(" Rien hahaha...")
     player.decrease_stats()
