0c8ff9a0848d5020121799a15c34a8709ba2ef2b
tompass8 <tom.passerman@ynov.com>
2025-10-30 11:44:24 +0100

    feat: Room avec zones chasse/pêche et affichage ASCII

--- src/room.py
+++ src/room.py
@@ -1,3 +1,5 @@
+from src.ascii_art import ROOM_ART
+
 class Room:
     def __init__(self, name, description, can_hunt=False, can_fish=False):
         self.name = name
@@ -11,12 +13,15 @@ class Room:
         self.connections[direction] = room
 
     def show_info(self):
-        print(f"\n Vous êtes dans {self.name}")
+        art = ROOM_ART.get(self.name, "")
+        if art:
+            print(art)
+        print(f"\nVous êtes dans {self.name}")
         print(self.description)
         if self.items:
             print("Objets présents :", ", ".join([i.name for i in self.items]))
         print("Sorties :", ", ".join(self.connections.keys()))
         if self.can_hunt:
-            print(" Ici, vous pouvez chasser.")
+            print("Vous pouvez chasser ici.")
         if self.can_fish:
-            print(" Ici, vous pouvez pêcher.")
+            print("Vous pouvez pêcher ici.")
